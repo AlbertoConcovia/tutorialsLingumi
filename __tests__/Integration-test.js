09a437d5b875793a7d90c137f9c6bfb207320081
albertoconcovia <concovia@gmail.com>
2021-05-20 15:26:42 +0100

    Axios installed
    Created Unit Test for call get of Ligumi webservice using mock implementation
    Integration-test.js
    
    Created call using axios to https://lingumitake-home-test-server.herokuapp.com/videoTutorials

---
+++ __tests__/Integration-test.js
@@ -0,0 +1,25 @@
+import axios from 'axios';
+import { fetchVideoTutorialsData, API } from '../webservices/videoTutorialsService';
+/**
+ * @aconcovia
+ * Unit Test for call get of Ligumi webservice using mock implementation
+ * @format
+ */
+
+jest.mock('axios');
+ 
+describe('fetchVideoTutorialsData', () => {
+  //Test to retrieve the data form Lingumi webservice
+  it('fetches successfully data from Lingumi API', async () => {
+    const data = {};
+ 
+    axios.get.mockImplementationOnce(() => Promise.resolve(data));
+ 
+    await expect(fetchVideoTutorialsData('react')).resolves.toEqual(data);
+ 
+    expect(axios.get).toHaveBeenCalledWith(
+      `${API}`,
+    );
+  });
+ 
+});
---
+++ webservices/videoTutorialsService.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const API = 'https://lingumitake-home-test-server.herokuapp.com/videoTutorials';
+ 
+export const fetchVideoTutorialsData = async query => {
+  const url = `${API}`;
+  return await axios.get(url);
+};
+ 
+fetchVideoTutorialsData('react');
